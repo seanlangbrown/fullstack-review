d79895231d27e7fafcc2811fdd609806262b6c22
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-16 15:53:57 -0800

    implement mongoDB schema and save methods

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,31 @@
 const request = require('request');
 const config = require('../config.js');
+const db = require('./database/index.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: 'https://api.github.com/users/' + username + '/repos',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+  request.get(options, (err, repos) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('repos received', repos);
+      db.save(repos.owner.name, repos.name, repos.url, repos.forks_count, repos.watchers_count,)
+    }
+  });
+  //.on('error', (err) => (console.log(err)))
+  //extract url, name, forks_count, watchers_count, size?, owner.login
 }
 
 module.exports.getReposByUsername = getReposByUsername;
