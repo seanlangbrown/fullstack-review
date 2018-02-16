d94975f9cdf77365fdfad99493604c77675bef80
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-16 14:57:07 -0800

    implement mongoDb schema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  username: String,
+  name: String,
+  rank: Number;
+  rankScore: Number;
+  commits: Number;
+  pullrequests: Number;
+  files; files;
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (username, name, commits, pullrequests, files, rankScore) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let repo = new Repo({name: name, username: username, commits: commits, pullrequests: pullrequests, files: files, rankscore: rankScore});
+  repo.save((err) => {
+    if (err) return console.error(err);
+  });
 }
 
 module.exports.save = save;
