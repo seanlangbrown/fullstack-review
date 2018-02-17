356dc2306ba534fbefe8f927144665c703ad2ba3
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-16 19:14:08 -0800

    complete repo save in mongoDB

--- database/index.js
+++ database/index.js
@@ -1,31 +1,36 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://127.0.0.1/fetcher');
 
 let repoSchema = mongoose.Schema({
-  id: Number,
-  username: String,
-  name: {
+  id: {
     type: Number, 
     index: true,
     unique: true
   },
-  rank: Number;
-  rankScore: Number;
-  forks: Number;
-  watchers: Number;
-  url: String;
+  username: String,
+  name: String,
+  rankScore: Number,
+  forks: Number,
+  watchers: Number,
+  url: String,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (username, name, forks, watchers, url, rankScore) => {
+let save = (id, username, name, forks, watchers, url, rankScore) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  let repo = new Repo({name: name, username: username, url: url, forks: forks, watchers: watchers, rankscore: rankScore});
+  console.log('saving repo in mongoDB');
+  let repo = new Repo({id: id, name: name, username: username, url: url, forks: forks, watchers: watchers, rankscore: rankScore});
   repo.save((err) => {
     if (err) return console.error(err);
   });
 }
 
+let getTopRepos = () => {
+  return;//Repo.find({});
+}
+
 module.exports.save = save;
+module.exports.getTopRepos = getTopRepos;
