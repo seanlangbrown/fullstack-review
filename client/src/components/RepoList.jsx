3ccab6af8dbdf7910a051eda82eef4c5d404641e
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-16 19:56:21 -0800

    implemented list of links to repos in repoList component

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import _ from 'lodash';
 
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    <ol>
+    {_.map(props.repos, (repo) => (<li key={repo.id}><a href={repo.url}>{repo.name}</a></li>))}
+    </ol>
   </div>
 )
 
